47c1687448b25f7eab1dfda1ecba68d8037550f6
Trikcode <ayesiganobert@gmail.com>
2022-04-22 20:39:05 +0300

    mysql advanced

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,4 @@
+-- script that lists all bands with Glam rock as their
+-- main style, ranked by their longevity
+SELECT band_name, COALESCE(split, 2021) - formed as lifespan FROM 
+metal_bands WHERE style LIKE '%Glam rock%';
