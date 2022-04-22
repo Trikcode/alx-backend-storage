0d50b67a0b8911544a7bc110c0963a1d06fe2adf
Trikcode <ayesiganobert@gmail.com>
2022-04-22 22:45:58 +0300

    advanced

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,23 +1,16 @@
--- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser 
--- that computes and store the average weighted score for a student.
+-- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser
+-- That computes and store the average weighted score for a student
 
--- Requirements:
-
--- Procedure ComputeAverageScoreForUser is taking 1 input:
--- user_id, a users.id value (you can assume user_id is linked to an existing users)
-
-DELIMITER ||
-CREATE PROCEDURE ComputerAverageScoreForUser (user_id INT)
-BEGIN DECLARE w_avg_Score FLOAT;
-
-SET w_avg_score = (
-   SELECT SUM(score * weight) / SUM(weight)
-FROM users AS user 
-JOIN corrections AS corr ON user.id=corr.user_id
-JOIN projects AS proj ON corr.project_id=proj.id 
-WHERE user.id=user_id); UPDATE users
-
-SET average_score = w_avg_score
-WHERE id=user_id;
-END ||
-DELIMITER ;
+DROP procedure IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER |
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (
+	IN user_id INT
+)
+BEGIN
+    UPDATE users
+   	SET average_score=(SELECT AVG(score) FROM corrections
+			     WHERE corrections.user_id=user_id
+			GROUP BY corrections.user_id)
+	WHERE id=user_id;
+END;
+| DELIMITER;
