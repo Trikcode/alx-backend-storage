1a105ad70e51e9b186ab1f1c790a28bcd4dd6b6d
Trikcode <ayesiganobert@gmail.com>
2022-04-27 20:17:39 +0300

    web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,21 +1,44 @@
 #!/usr/bin/env python3
 """
-create a web cach
+Contains the definition of the get_page function
 """
 import redis
 import requests
-rc = redis.Redis()
-count = 0
+from functools import wraps
+from typing import Callable
 
 
+def cached(func: Callable) -> Callable:
+    """
+    Decorator that caches the results of the function call
+    """
+    red = redis.Redis()
+
+    @wraps(func)
+    def wrapper(url):
+        """
+        cache results of func and keep track of how many times url is accessed
+        """
+        red.incr(f"count:{url}")
+        content = red.get(f"{url}")
+        if content:
+            return content.decode("utf-8")
+        content = func(url)
+        red.setex(f"{url}", 10, content)
+        return content
+    return wrapper
+
+
+@cached
 def get_page(url: str) -> str:
-    """ get a page and cach value"""
-    rc.set(f"cached:{url}", count)
-    resp = requests.get(url)
-    rc.incr(f"count:{url}")
-    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
-    return resp.text
-
-
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    """
+    Obtain the html content of a particular URL and return it.
+    Track how many times a URL was accessed in the key 'count: {url}'
+    Cache the result with an expiration time of 10 seconds
+    Args:
+        url (str): url to be accessed
+    Return:
+        html content from the accessed url
+    """
+    response = requests.get(url)
+    return response.text
